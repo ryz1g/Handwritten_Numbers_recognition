683ae50296358590ef0b4e3205b8a6b0366030ab
ryz1g <65825273+ryz1g@users.noreply.github.com>
2020-10-29 15:27:19 +0530

    Update trial.py

--- trial.py
+++ trial.py
@@ -1,25 +1,31 @@
 import cv2
 import PIL
 import helping_functions as hf
-import numpy as np
 
 v=cv2.VideoCapture(0)
 _, frame=v.read()
+flag=0
 while(True):
     ret, frame=v.read()
-    #c1,c2=frame_prev.shape
     frame=hf.new_rectifier(cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY))
-    #frame=cv2.line(frame,(0,84), (399,84), (255,0,0), 1)
-    #frame=cv2.line(frame,(0,140), (399,140), (255,0,0), 1)
-    #img=PIL.Image.fromarray(frame).crop((0, 85, 399, 140))
     img=PIL.Image.fromarray(frame)
-    op=hf.output_multiple(img)
-    frame=cv2.putText(frame,str(op),(0,105), cv2.FONT_HERSHEY_SIMPLEX, 0.75,(150,150,150),2,cv2.LINE_AA)
-    cv2.imshow('Testing: Press Q to quit', frame)
-    if cv2.waitKey(1) & 0xFF==ord('q'):
-        #img.show()
-        #op=hf.output_multiple(img)
+    ke=cv2.waitKey(1)
+    if ke==ord('q'):
         break
+    if ke==ord('s'):
+        cv2.destroyAllWindows()
+        flag=1
+    if ke==ord('d'):
+        cv2.destroyAllWindows()
+        flag=0
+    if flag==1:
+        op=hf.output_multiple(img)
+        frame=cv2.putText(frame,str(op),(0,105), cv2.FONT_HERSHEY_SIMPLEX, 0.75,(150,150,150),2,cv2.LINE_AA)
+        cv2.imshow('Testing: S:Start Detection | D:Stop Detection| Q:Quit', frame)
+    if flag==0:
+        cv2.imshow('Testing: Press Q to quit', frame)
+
 
 v.release()
 cv2.destroyAllWindows()
+
